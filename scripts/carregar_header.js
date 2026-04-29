aef4e6a0bbfd6752a1288193651fe0e79f96da52
Felipe Silva <felipeconceicaodasilva@hotmail.com>
2026-04-28 23:39:06 -0300

    Estoque persistente
    Estoque agora é persistente (localStorage), e a lógica do span do número do carrinho agora é carregado no carregar_header.js ao invés de em um script separado.

--- scripts/carregar_header.js
+++ scripts/carregar_header.js
@@ -33,3 +33,16 @@ headerTag.insertAdjacentHTML('afterbegin', `
                 </a>
             </div>
         </nav>`)
+
+const span = document.getElementById('span_carrinho');
+
+function atualizar() {
+    let obj = JSON.parse(sessionStorage.getItem('carrinho')) || 0;
+    if (obj === 0 || obj.length === 0) {
+        span.innerText = '';
+    } else {
+        span.innerText = obj.length;
+    };
+};
+
+atualizar();
